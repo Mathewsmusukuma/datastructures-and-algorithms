2123ef9bf8370f930570d09228ff4fa96d144157
Mathewsmusukuma <sikaili99@gmail.com>
2022-01-07 16:25:28 +0200

    Added new DSA

--- maximum_product_subarray.py
+++ maximum_product_subarray.py
@@ -3,9 +3,12 @@ def max_product(nums):
     currentMin, currentMax = 1, 1
 
     for n in nums:            
-        temp = currentMax * n
-        currentMax = max(n * currentMax, n * currentMin, n)
+        temp = n * currentMax
         currentMin = min(temp, n * currentMin, n)
+        currentMax = max(n * currentMax, n * currentMin, n)
+        
         result = max(result,currentMax)
 
     return result
+
+print(max_product([2,3,4,5,6,7,8]))
